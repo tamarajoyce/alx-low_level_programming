1cc33c58a134b251c7ce0074a863f881a6e99949
tamarajoyce <tamarajoyce03@gmail.com>
2022-12-14 17:49:53 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,7 +27,7 @@ int main(void)
 	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
-	half1 = fib1_half1 + fib2_half2;
+	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
 	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
