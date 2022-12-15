b9e6c7ecda74dbfda04af96c6696de75b9caabe5
tamarajoyce <tamarajoyce03@gmail.com>
2022-12-15 05:17:00 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,16 +6,19 @@
  */
 
 int main(void)
-
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
-	printf("infinite loop avoied! \\o/\n");
+
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
