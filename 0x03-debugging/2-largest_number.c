e55bbb77e1d083205c9a8d9ef45f4493fab107a9
tamarajoyce <tamarajoyce03@gmail.com>
2022-12-15 08:44:33 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,16 +8,17 @@
  * Return: largest number
  */
 
+
 int largest_number(int a, int b, int c)
 
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b >= a && b >= c)
 	{
 	largest = b;
 	}
@@ -26,4 +27,5 @@ int largest_number(int a, int b, int c)
 	largest = c;
 	}
 	return (largest);
+
 }
